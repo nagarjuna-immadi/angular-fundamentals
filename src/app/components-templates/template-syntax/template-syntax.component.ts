fb637e128dfc0bd0f7e77d5a1f073361d3fc7cec
Nagarjuna Immadi <nagarjuna.immadi@thomsonreuters.com>
2018-07-05 11:10:33 +0530

    Property binding compleated

--- src/app/components-templates/template-syntax/template-syntax.component.ts
+++ src/app/components-templates/template-syntax/template-syntax.component.ts
@@ -26,6 +26,8 @@ export class TemplateSyntaxComponent implements OnInit {
     'special':  this.isSpecial
   };
 
+  evilTitle = 'Template <script>alert("evil never sleeps")</script>Syntax';
+
   constructor() { }
 
   ngOnInit() {
@@ -33,6 +35,13 @@ export class TemplateSyntaxComponent implements OnInit {
 
   getVal() {
     return 1;
+  }
+
+  getHeroImage() {
+    // this.formNotChanged = false; // Un-comment me to see error in console.
+    //getHeroImage() method is used in view to get the hero image. If we modify the value of formNotChanged property in this function we will get an error in console. We should not do like this.
+    // getHeroImage() method should only return a value. It should not modify any other component property.
+    return this.heroImageUrl;
   }
 
 
